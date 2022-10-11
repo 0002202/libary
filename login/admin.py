73bf44f9c64fa58ac9a3e7f5054433c1d145fe26
nbdj_xyz <3177765083@qq.com>
2022-10-11 09:37:42 +0800

    test_commit

--- login/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Student
-
-
-# 将模型提交给管理员进行管理
-admin.site.register(Student)
--- seat/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import StandardSeat, StudyRoomSeat, LeisureSeat
-
-admin.site.register(StandardSeat)
-admin.site.register(StudyRoomSeat)
-admin.site.register(LeisureSeat)
